57cd114a7d003475f67358ed65e5dc5ed162caa5
Authority6789 <authorityeric6@gmail.com>
2021-12-17 05:14:15 -0800

    initial commit

---
+++ controllers/todoController.js
@@ -0,0 +1,58 @@
+const todoModel = require('../models/todoModel');
+
+ async function addTodo(request,response){
+   try {
+    const newTodo = await todoModel.create(request.body);
+    response.status(200).json(newTodo);
+   } catch (error) {
+       console.log("Something went wrong",error.message);
+   }
+}
+
+async function getAllTodo(request,response){
+try {
+  const getAll =await todoModel.find();
+  response.status(200).json(getAll) ;
+} catch (error) {
+  console.log('something went wrong',error.message); 
+  response.status(400) 
+}
+}
+
+
+async function getTodoById(request,response){
+    try {
+     const get1todo =await todoModel.findById(request.params.todoId);
+     response.status(200).json (get1todo);  
+    } catch (error) {
+     console.log('something went wrong',error.message);   
+    }
+}
+
+async function updateById (request,response){
+    try {
+     const updateId=await todoModel.findByIdAndUpdate(request.params.todoId,request.body);
+     response.status(200).json(updateId);
+    }
+     catch (error) {
+      console.log('something wwent wrong',error.message); 
+    }
+
+}
+async function deleteById(request,response){
+try {
+  await todoModel.findByIdAndDelete(request.params.todoId); 
+  response.status(200).json({message:"todo deleted"});
+} catch (error) {
+    console.log("something went wrong",error.message);
+    
+}
+}
+
+module.exports ={
+    addTodo,
+    getAllTodo,
+    updateById,
+    deleteById,
+    getTodoById
+}
---
+++ server.js
@@ -0,0 +1,37 @@
+const { response, request } = require('express');
+const express = require('express');
+const app = express();
+const PORT =process.env.PORT || 2245;
+//importing mongoose 
+const mongoose = require('mongoose');
+// importing todoController
+const todoController = require('./controllers/todoController');
+
+app.use(express.json());
+
+
+app.get('/',(request,respon)=>{
+    response.status(200).json({Message:"hello welcome to my todo APT"});
+}
+)
+
+app.post('/todo',todoController.addTodo);
+app.get ('/todo',todoController.getAllTodo);
+app.put('/todo',todoController.updateById);
+app.delete('/todo',todoController.deleteById);
+app.get('/todo/:todoId',todoController.getTodoById);
+
+// listening to request on localhost port 5010
+app.listen(PORT,() => {
+    console.log("My server is Up and running on port:",PORT);
+    // connecting to the database
+    mongoose.connect(process.env.DB_URL)
+    .then(function(){
+        console.log("DataBase is connected");
+    })
+    .catch(function(error){
+        // console.log("DataBase not connect",error);
+        console.log("DataBase not connected" ,error);
+    });
+});
+
